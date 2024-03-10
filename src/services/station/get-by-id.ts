ba6aa993007734cde15d3e978c28bc56ea6c0995
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-03-10 16:57:27 +0700

    rename

--- src/services/station/get-by-id.ts
+++ src/services/station/get-by-id.ts
@@ -2,14 +2,14 @@ import { sql } from "drizzle-orm"
 import { db } from "../../db"
 import { logger } from "../../utils/log"
 
-export const getItemById = async (id: string) => {
+export const getItemById = async (stationId: string) => {
   try {
     const stations = await db.execute(
-      sql`SELECT * FROM station WHERE id = ${id}`
+      sql`SELECT * FROM station WHERE id = ${stationId}`
     )
 
     if (stations.length === 0) {
-      logger.error(`[QUERY][STATION][${id}] Station data is not found`)
+      logger.error(`[QUERY][STATION][${stationId}] Station data is not found`)
       return null
     }
 
