5a5dfd4a6f19dd94704e6af353a29deaa7194c9a
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-03-11 17:44:41 +0700

    add indexes

---
+++ src/db/migrations/0002_smart_vermin.sql
@@ -0,0 +1,1 @@
+CREATE INDEX IF NOT EXISTS "station_idx" ON "schedule" ("station_id");
--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -4,6 +4,7 @@ import {
   bigserial,
   boolean,
   date,
+  index,
   integer,
   pgEnum,
   pgTable,
@@ -12,18 +13,26 @@ import {
   uuid,
 } from "drizzle-orm/pg-core"
 
-export const schedule = pgTable("schedule", {
-  id: text("id").primaryKey().unique(),
-  stationId: text("station_id").default(sql`NULL`),
-  trainId: text("train_id").default(sql`NULL`),
-  line: text("line").default(sql`NULL`),
-  route: text("route").default(sql`NULL`),
-  color: text("color").default(sql`NULL`),
-  destination: text("destination").default(sql`NULL`),
-  timeEstimated: time("time_estimated").default(sql`NULL`),
-  destinationTime: time("destination_time").default(sql`NULL`),
-  updatedAt: text("updated_at").default(sql`(CURRENT_TIMESTAMP)`),
-})
+export const schedule = pgTable(
+  "schedule",
+  {
+    id: text("id").primaryKey().unique(),
+    stationId: text("station_id").default(sql`NULL`),
+    trainId: text("train_id").default(sql`NULL`),
+    line: text("line").default(sql`NULL`),
+    route: text("route").default(sql`NULL`),
+    color: text("color").default(sql`NULL`),
+    destination: text("destination").default(sql`NULL`),
+    timeEstimated: time("time_estimated").default(sql`NULL`),
+    destinationTime: time("destination_time").default(sql`NULL`),
+    updatedAt: text("updated_at").default(sql`(CURRENT_TIMESTAMP)`),
+  },
+  (table) => {
+    return {
+      stationIdx: index("station_idx").on(table.stationId),
+    }
+  }
+)
 
 export type Schedule = typeof schedule.$inferSelect
 
