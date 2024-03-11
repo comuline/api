182264b005ab75174961fe098d58f1175842d7df
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-03-11 19:09:02 +0700

    Revert "set to gmt+7"
    This reverts commit 6bad19944452036c3dd438934c05e0f15d7181b0.

--- src/commons/utils/date.ts
+++ src/commons/utils/date.ts
@@ -5,7 +5,5 @@ export function parseTime(timeString: string): Date {
   date.setMinutes(minutes ?? date.getMinutes())
   date.setSeconds(seconds ?? date.getSeconds())
 
-  date.setUTCHours(date.getUTCHours() + 7)
-
   return date
 }
