fdc994d3a31600395881b65e22f15675c4beed3b
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-03-21 04:38:56 +0700

    add controller

--- src/services/route/index.ts
+++ src/services/route/index.ts
@@ -1,9 +1,21 @@
 import { NotFoundError } from "elysia"
-import { getAll } from "./get-all"
+import { getAll, getAllFrom } from "./get-all"
 
 export const route = {
-  getAll: async (trainId: string, fromStationId?: string) => {
-    const routes = await getAll(trainId, fromStationId)
+  getAll: async (trainId: string) => {
+    const routes = await getAll(trainId)
+
+    if (!routes) {
+      throw new NotFoundError("Route data is not found")
+    }
+
+    return {
+      status: 200,
+      data: routes,
+    }
+  },
+  getAllFrom: async (trainId: string, fromStationId?: string) => {
+    const routes = await getAllFrom(trainId, fromStationId)
 
     if (!routes) {
       throw new NotFoundError("Route data is not found")
