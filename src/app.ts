72820c202d57a8640e5ffed9a010d781a9660352
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-09-10 15:02:18 +0700

    init hono, openapi, scalar

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,84 @@
-import { Context } from "elysia"
-import { app } from "./index"
-import { Env } from "./types"
-import Container from "typedi"
-import { createDB } from "./db"
+import { OpenAPIHono, z } from "@hono/zod-openapi"
+import { Hono } from "hono"
+import { createRoute } from "@hono/zod-openapi"
 
-export default {
-  async fetch(request: Request, env: Env, ctx: Context): Promise<Response> {
-    const db = createDB(env.DATABASE_URL)
-    Container.set("env", env)
-    Container.set("db", db)
-    Container.set("kv", env.KV)
-    return await app.fetch(request)
+import { apiReference } from "@scalar/hono-api-reference"
+
+const ParamsSchema = z.object({
+  id: z
+    .string()
+    .min(3)
+    .openapi({
+      param: {
+        name: "id",
+        in: "path",
+      },
+      example: "1212121",
+    }),
+})
+
+const UserSchema = z
+  .object({
+    id: z.string().openapi({
+      example: "123",
+    }),
+    name: z.string().openapi({
+      example: "John Doe",
+    }),
+    age: z.number().openapi({
+      example: 42,
+    }),
+  })
+  .openapi("User")
+
+const route = createRoute({
+  method: "get",
+  path: "/users/{id}",
+  request: {
+    params: ParamsSchema,
   },
-}
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: UserSchema,
+        },
+      },
+      description: "Retrieve the user",
+    },
+  },
+})
+
+const app = new OpenAPIHono()
+
+app.use(
+  "/reference",
+  apiReference({
+    cdn: "https://cdn.jsdelivr.net/npm/@scalar/api-reference",
+    spec: {
+      url: "/doc",
+    },
+  }),
+)
+
+app.get("/", (c) => c.json({ status: "ok" }))
+
+app.openapi(route, (c) => {
+  const { id } = c.req.valid("param")
+  return c.json({
+    id,
+    age: 20,
+    name: "Ultra-man",
+  })
+})
+
+// The OpenAPI documentation will be available at /doc
+app.doc("/doc", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "My API",
+  },
+})
+
+export default app
